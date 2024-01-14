ba2167fe7c043c7c81abd8f447f9938e448a04bb
Shubham Arora <shubhamarora@Shubhams-MacBook-Pro.local>
2024-01-14 12:52:03 +0530

    Removing unecessary files

--- scripts/local/install_dependencies.sh
+++ scripts/local/install_dependencies.sh
@@ -4,9 +4,6 @@ export INSTALL_ADDITIONAL_DEPENDENCIES="test, dev"
 
 export MODULE_NAME="distributed_locking_service"
 
-# run the following command the first time, the repo is initialized
-#git submodule add git@bitbucket.org:airterra-code/cicd-tools.git cicd-tools
-
 git submodule update --init --recursive cicd-tools
 
 ./cicd-tools/build/shared/install_proj_requirements.sh
