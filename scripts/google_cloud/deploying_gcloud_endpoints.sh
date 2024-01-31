a2d9bbb3462908590d1f992759f4e1b17a7d71f4
Shubham Arora <shubhamarora@Shubhams-MacBook-Pro.local>
2024-01-31 16:22:39 +0530

    Updating readme.

--- scripts/google_cloud/deploying_gcloud_endpoints.sh
+++ scripts/google_cloud/deploying_gcloud_endpoints.sh
@@ -12,12 +12,14 @@ gcloud endpoints services deploy endpoint-service-definition.yaml \
 
 # run the command below only for the first time
 #chmod +x scripts/google_cloud/gcloud_build_image
+#gcloud services enable servicemanagement.googleapis.com
+#gcloud services enable servicecontrol.googleapis.com
 
 ./gcloud_build_image.sh -s cloud-endpoint-proxy-application-z4ndkzhdma-uc.a.run.app \
-    -c 2024-01-31r0 -p prefab-sky-412817
+    -c 2024-01-31r2 -p prefab-sky-412817
 
 gcloud run deploy cloud-endpoint-proxy-application \
-  --image="gcr.io/prefab-sky-412817/endpoints-runtime-serverless:2.46.0-cloud-endpoint-proxy-application-z4ndkzhdma-uc.a.run.app-2024-01-31r0" \
+  --image=gcr.io/prefab-sky-412817/endpoints-runtime-serverless:2.46.0-cloud-endpoint-proxy-application-z4ndkzhdma-uc.a.run.app-2024-01-31r2 \
   --allow-unauthenticated \
   --platform managed \
   --project=prefab-sky-412817 \
